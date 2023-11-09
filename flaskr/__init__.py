986f9f354db830614290cde799d03723c1fc8cb1
SeriousCoffeeCup <150287248+SeriousCoffeeCup@users.noreply.github.com>
2023-11-09 22:20:59 +0000

    Stuck at flaskr unknown word, pyfile unknown word

---
+++ flaskr/__init__.py
@@ -0,0 +1,34 @@
+import os 
+
+from flask import Flash
+
+#I'm not sure if I am meant to digest or break this somehow
+
+def create_app(test_config = None):
+    # creating and configuring the app
+    app = Flash(__name__, instance_relative_config=True)
+        #curr python module = __name__
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
